85da5f0820bbd4f88e10b1936bd112c644c35a55
gustavocambareri <gustavo.cambareri@gmail.com>
2022-11-22 22:26:39 +0100

    Creation

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# P5-empty-project
-This repository is the starting point of the assignments given in the elective course [Creative Coding at Politecnico di Milano](https://www11.ceda.polimi.it/schedaincarico/schedaincarico/controller/scheda_pubblica/SchedaPublic.do?&evn_default=evento&c_classe=696598&__pj0=0&__pj1=3ed8420c42c849845b5caa3de626e8fc).
-Browse [this website](https://drawwithcode.github.io/) if you want to know more about it.
+This is a sci-fi game for kids. Interaction is simple : clicking is enough. Different sci-fi sound designs produced by me are played in the sketch randomly, depending on the randomFactor variable, which changes every other click.
--- sketch.js
+++ sketch.js
@@ -1,16 +1,40 @@
+// init sound array
+let suoni = [];
+
 function preload() {
-	// put preload code here
+  // put preload code here
+  clickSound = loadSound("./assets/sound1.mp3");
+  clickSound2 = loadSound("./assets/sound2.mp3");
+  clickSound3 = loadSound("./assets/sound3.mp3");
+  clickSound4 = loadSound("./assets/sound4.mp3");
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  // define sound array
+
+  suoni = [clickSound, clickSound2, clickSound3, clickSound4];
 }
 
-function draw() {
-	// put drawing code here
+function mouseClicked() {
+  //sound and color changes randomly
+  noStroke();
+  let randomFactor = Math.floor(Math.random() * 4);
+  console.log(randomFactor);
+  if (randomFactor === 0) {
+    fill(247, 0, 132);
+  } else if (randomFactor === 1) {
+    fill(100, 100, 220);
+  } else if (randomFactor === 2) {
+    fill(228, 39, 71);
+  } else if (randomFactor === 3) {
+    fill(31, 63, 114);
+  }
+
+  creaCerchio();
+  suoni[randomFactor].play();
+}
+
+function creaCerchio() {
+  circle(random(0, width), random(0, height), random(0, 500));
 }
